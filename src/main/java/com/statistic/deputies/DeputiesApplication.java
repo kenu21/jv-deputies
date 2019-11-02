7cb4695e3f3b454b028db4e8c587f01716b5dcd7
Iurii <kenu21@gmail.com>
2019-11-02 13:51:00 +0200

    init

---
+++ src/main/java/com/statistic/deputies/DeputiesApplication.java
@@ -0,0 +1,11 @@
+package com.statistic.deputies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeputiesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DeputiesApplication.class, args);
+    }
+}
---
+++ src/test/java/com/statistic/deputies/DeputiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.statistic.deputies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeputiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
