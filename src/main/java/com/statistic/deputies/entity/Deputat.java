976b6b7446d8a84a209c63ffed96ef8c1ab1742f
Iurii <kenu21@gmail.com>
2019-11-05 14:24:21 +0200

    parsed data

---
+++ src/main/java/com/statistic/deputies/entity/Deputat.java
@@ -0,0 +1,38 @@
+package com.statistic.deputies.entity;
+
+import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "deputies")
+public class Deputat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer rada;
+    private String name;
+    private String party;
+    private LocalDate startWork;
+    private LocalDate endWork;
+    private String nationality;
+
+    @Column(columnDefinition = "TEXT")
+    private String activity;
+
+    @Column(columnDefinition = "TEXT")
+    private String awards;
+
+    @Column(columnDefinition = "TEXT")
+    private String edu;
+}
---
+++ src/main/java/com/statistic/deputies/repository/DeputatRepository.java
@@ -0,0 +1,6 @@
+package com.statistic.deputies.repository;
+
+import com.statistic.deputies.entity.Deputat;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeputatRepository extends JpaRepository<Deputat, Long> {}
