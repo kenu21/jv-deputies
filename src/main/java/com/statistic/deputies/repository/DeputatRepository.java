c907a984b026ce8021d7ac97f06b7b10980cd5e2
Iurii <kenu21@gmail.com>
2019-11-11 20:58:45 +0200

    added getting all deputies and group by party

--- src/main/java/com/statistic/deputies/repository/DeputatRepository.java
+++ src/main/java/com/statistic/deputies/repository/DeputatRepository.java
@@ -1,6 +1,8 @@
 package com.statistic.deputies.repository;
 
 import com.statistic.deputies.entity.Deputat;
+
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -14,4 +16,9 @@ public interface DeputatRepository extends JpaRepository<Deputat, Long> {
     List<Deputat> findByActivityNotLike(String activity);
 
     List<Deputat> findByAwardsNot(String awards);
+
+    @Query("SELECT d FROM Deputat d WHERE d.startWork >= :startWork"
+            + " AND d.endWork <= :endWork GROUP BY d.party, d.id")
+    List<Deputat> getAllDeputiesGroupByParty(@Param("startWork") LocalDate startWork,
+                                             @Param("endWork") LocalDate endWork);
 }
